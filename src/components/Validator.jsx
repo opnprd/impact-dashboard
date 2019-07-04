eca886ab8af577c5a7e22c1c6c9cde097e594284
Giles Dring <gilesdring@gmail.com>
2019-07-04 11:48:30 +0100

    Add documentation show/hide

--- src/components/Validator.jsx
+++ src/components/Validator.jsx
@@ -1,11 +1,28 @@
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import syndicationBlurb from './text/syndicationFormat.md';
 
+function ShowHide({ id, label }) {
+  return <>
+    <input type='checkbox' id={ id } name={ id } />
+    <label htmlFor={ id } >{ label }</label>
+  </>;
+}
+ShowHide.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
 export default function Validator() {
-  return <><section
-    dangerouslySetInnerHTML={{ __html: syndicationBlurb }}
-  >
-  </section></>;
+  return <>
+    <ShowHide id='show-doc' label='Show documentation'/>
+    <section id='documentation'>
+      <h1>Documentation</h1>
+      <section
+        dangerouslySetInnerHTML={{ __html: syndicationBlurb }}
+      />
+    </section>
+  </>;
 }
